6eea6432848d63c74010def3b1ad70436d210fa6
Babacar Thiam <141820773+babacarth7@users.noreply.github.com>
2024-07-05 17:40:51 +0000

    (fix) quick fix

--- src/main/java/com/babacarthiam/springboot_auth_api_v1/services/UserService.java
+++ src/main/java/com/babacarthiam/springboot_auth_api_v1/services/UserService.java
@@ -1,5 +1,8 @@
 package com.babacarthiam.springboot_auth_api_v1.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.ArrayList;
@@ -13,15 +16,15 @@ import com.babacarthiam.springboot_auth_api_v1.entities.RoleEnumeration;
 import com.babacarthiam.springboot_auth_api_v1.entities.UserEntity;
 import com.babacarthiam.springboot_auth_api_v1.repositories.RoleRepository;
 import com.babacarthiam.springboot_auth_api_v1.repositories.UserRepository;
-
 @Service
 public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository,
-            RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
@@ -29,24 +32,23 @@ public class UserService {
 
     public List<UserEntity> allUserEntities() {
         List<UserEntity> userEntities = new ArrayList<>();
-
         userRepository.findAll().forEach(userEntities::add);
-
         return userEntities;
     }
 
     public UserEntity createAdministrator(RegisterUserDto input) {
-       Optional<RoleEntity> optionalRole = roleRepository.findByName(RoleEnumeration.ADMIN);
+        Optional<RoleEntity> optionalRole = roleRepository.findByName(RoleEnumeration.ADMIN);
 
         if (optionalRole.isEmpty()) {
-            return null;
+            logger.error("Role ADMIN not found in database");
+            throw new RuntimeException("Role ADMIN not found");
         }
 
         var user = new UserEntity().setFullName(input.getFullName())
                 .setEmail(input.getEmail())
                 .setPassword(passwordEncoder.encode(input.getPassword()))
                 .setRoleEntity(optionalRole.get());
-        
+
         return userRepository.save(user);
     }
 }
